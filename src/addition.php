ab96909b20f3650f3fbb16e8cdf91156679e9332
rkadam01 <rkadam01@xento.com>
2020-07-11 22:02:07 +0530

    first commit

--- src/addition.php
+++ src/addition.php
@@ -9,7 +9,7 @@ class Addition
 	public function getAddition( $data ) {
 		$firstElement = array_shift($data);
 		if( false == ctype_digit(implode('',$data))){
-			return 'Please enter the valid parameters.';
+			return 'Please enter the space seprated numbers.';
 		} else {
 			return 'The addition of numbers is: ' . array_sum($data);
 		}
